00e83a845256880da21e770d44277154069474e0
Prototype1 <>
2015-10-18 12:40:32 +0200

    Add example how to add another widgetset

--- SimpleVaadin/src/main/java/com/simplevaadin/ui/MyUi.java
+++ SimpleVaadin/src/main/java/com/simplevaadin/ui/MyUi.java
@@ -1,27 +1,48 @@
 package com.simplevaadin.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.alump.masonry.MasonryDnDWrapper;
 
 import com.simplevaadin.servlet.mapper.UserMapper;
+import com.vaadin.annotations.Theme;
+import com.vaadin.event.dd.DragAndDropEvent;
+import com.vaadin.event.dd.DropHandler;
+import com.vaadin.event.dd.acceptcriteria.AcceptCriterion;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+
+import fi.jasoft.dragdroplayouts.drophandlers.DefaultGridLayoutDropHandler;
 
 @SpringUI
+@Theme("valo")
 public class MyUi extends UI {
 
 	@Autowired
 	UserMapper userService;
+	public static final String NAME = "dd-grid-layout";
+
+	private static final int ROWS = 4;
+	private static final int COLUMNS = 4;
 
 	@Override
 	protected void init(VaadinRequest request) {
+		MasonryDnDWrapper layout = new MasonryDnDWrapper();
 
-		VerticalLayout layout = new VerticalLayout();
-		// just add a new Label with ButtonClicked on Click
-		layout.addComponent(new Button("Click", e -> layout.addComponent(new Label(userService.selectUser() + ""))));
+		layout.setReorderable(true);
+		
+		layout.setDropHandler(new DefaultGridLayoutDropHandler());
+		
+		layout.addComponentToLayout(new Button("xxx"));
+
+		layout.addComponentToLayout(new Button("asd"));
+
+		layout.addComponentToLayout(new Button("sd"));
+
+		layout.addComponentToLayout(new Button("hfg"));
+
+		layout.addComponentToLayout(new Button("cxv"));
 
 		setContent(layout);
 
